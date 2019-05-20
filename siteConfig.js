75bdc9732df6658dcaf10c712fe208fde7c98058
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-05-20 11:31:04 -0400

    updated docs

--- siteConfig.js
+++ siteConfig.js
@@ -23,7 +23,7 @@
 const siteConfig = {
   title: 'IIIF Workshop', // Title for your website.
   tagline: 'Yet another set of IIIF Workshop guidelines',
-  url: 'https://jeffreycwitt.github.io', // Your website URL
+  url: 'http://jeffreycwitt.com', // Your website URL
   baseUrl: '/IIIFWorkshop/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
